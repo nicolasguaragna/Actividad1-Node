453faf605d4c02596f7431ac9cd65a1b8ccdea43
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-04-02 14:56:04 -0300

    update calculos.js

--- calculos.js
+++ calculos.js
@@ -14,3 +14,10 @@ function dividir(num1, num2) {
 
 console.log("división de 20 y 2:", dividir(20, 2));
 console.log("división de 20 y 0:", dividir(20, 0));
+
+function encontrarMayor(array) {
+    return Math.max(...array);
+}
+
+console.log("el mayor de la lista es:", encontrarMayor([2, 8, 9, 7, 5, 6]));
+
