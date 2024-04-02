1a319324ec62e9671baf7ee1ee19f45657c71290
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-04-02 14:55:02 -0300

    update calculos.js

---
+++ calculos.js
@@ -0,0 +1,16 @@
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+console.log("suma de 5 y 10:", sumar(5, 10));
+
+function dividir(num1, num2) {
+    if (num2 === 0) {
+        return "Error: No se puede dividir por cero";
+    } else {
+        return num1 / num2;
+    }
+}
+
+console.log("división de 20 y 2:", dividir(20, 2));
+console.log("división de 20 y 0:", dividir(20, 0));
